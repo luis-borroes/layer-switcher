96cfc2bc6d3f29a4885d0c329138e03ca7a22f5e
Pedro <pedroborroes@gmail.com>
2013-12-19 18:57:15 +0000

    added a proper aabb collision checking function

--- utils.py
+++ utils.py
@@ -9,3 +9,18 @@ class Utils(object):
 		elif num < target:
 			return min(num + step * dt * 100, target)
 		return num
+
+	def collide(self, rect1, rect2):
+		if rect1.left > rect2.right:
+			return False
+
+		if rect1.right < rect2.left:
+			return False
+
+		if rect1.top > rect2.bottom:
+			return False
+
+		if rect1.bottom < rect2.top:
+			return False
+
+		return True
