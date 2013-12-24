7a396bbae77faeca59cc380db58a85edcef6157b
Pedro <pedroborroes@gmail.com>
2013-12-24 02:02:03 +0000

    draw behind layers, added swimming, slow (speed modifier), kill blocks

--- player.py
+++ player.py
@@ -20,6 +20,7 @@ class Player(pygame.sprite.Sprite):
 
 		self.position = pygame.rect.Rect((0, 0), self.image.get_size())
 		self.acceleration = Vector(0, 0)
+		self.speedModifier = 1
 		self.resting = False
 		self.jumping = False
 		self.jumpTimer = 0
@@ -27,6 +28,7 @@ class Player(pygame.sprite.Sprite):
 		self.key_w = False
 		self.key_s = False
 		self.layer = 0
+		self.drawLayer = 0
 		self.layerChanging = False
 		self.layerCooldown = 0
 		self.oldAccel = 0
@@ -37,6 +39,7 @@ class Player(pygame.sprite.Sprite):
 				self.position.y = obj.y - (self.rect.height - self.map.tileheight)
 				if hasattr(obj, "layer"):
 					self.layer = int(obj.layer)
+					self.drawLayer = self.layer
 
 		self.layerOffset = 70 * self.layer
 
@@ -73,6 +76,7 @@ class Player(pygame.sprite.Sprite):
 
 				if not walled:
 					self.layer += 1
+					self.drawLayer = self.layer
 					self.layerChanging = True
 					self.acceleration.y = 0
 					self.layerCooldown = 0.5
@@ -81,24 +85,28 @@ class Player(pygame.sprite.Sprite):
 
 		if keys[pygame.K_a]:
 			self.acceleration.x = util.approach(dt, self.acceleration.x, -400, 20)
+
 		if keys[pygame.K_d]:
 			self.acceleration.x = util.approach(dt, self.acceleration.x, 400, 20)
 
 		if self.spaced:
 			self.spaced = False
 
-			if not self.layerChanging and not self.jumping and self.resting:
+			if not self.layerChanging and not self.jumping and not self.swimming and self.resting:
 				self.jumping = True
 				self.acceleration.y = -600
 				self.resting = False
 				self.spaced = False
 
-		if keys[pygame.K_SPACE] and self.jumping:
-			self.jumpTimer += 1
-			self.acceleration.y = util.approach(dt, self.acceleration.y, -600, 20)
-			if self.jumpTimer == 35:
-				self.jumping = False
-				self.jumpTimer = 0
+		if keys[pygame.K_SPACE]:
+			if self.jumping:
+				self.jumpTimer += 1
+				self.acceleration.y = util.approach(dt, self.acceleration.y, -600, 20)
+				if self.jumpTimer == 35:
+					self.jumping = False
+					self.jumpTimer = 0
+			elif self.swimming:
+				self.acceleration.y = util.approach(dt, self.acceleration.y, -300, 50)
 		elif self.jumping:
 			self.jumping = False
 
@@ -107,8 +115,10 @@ class Player(pygame.sprite.Sprite):
 		elif not self.layerChanging:
 			self.acceleration.x = util.approach(dt, self.acceleration.x, 0, 10)
 
-		self.position.x += int(round(self.acceleration.x * dt))
-		self.position.y += int(round(self.acceleration.y * dt))
+		self.position.x += int(round(self.acceleration.x * dt * self.speedModifier))
+		self.position.y += int(round(self.acceleration.y * dt * self.speedModifier))
+
+		self.speedModifier = 1
 
 		if self.position.x < 0:
 			self.position.x = 0
@@ -116,11 +126,13 @@ class Player(pygame.sprite.Sprite):
 
 		if self.position.right > game.map.width:
 			self.position.right = game.map.width
+			self.acceleration.x = 0
 
-		if self.position.y > game.map.height + 200:
+		if self.position.y > game.map.height + 250:
 			self.spawn()
 
 		self.resting = False
+		self.swimming = False
 
 		if self.layerChanging:
 			self.resting = True
@@ -131,6 +143,7 @@ class Player(pygame.sprite.Sprite):
 				self.layerChanging = False
 				self.resting = False
 				self.acceleration.y = self.oldAccel
+				self.drawLayer = self.layer
 
 			self.position.y += self.layerOffset - self.oldOff
 		else:
@@ -139,33 +152,44 @@ class Player(pygame.sprite.Sprite):
 		self.oldOff = self.layerOffset
 
 		for block in game.map.layers[self.layer]:
-			if block.collidable and util.collide(self.position, block.position):
+			if util.collide(self.position, block.position):
 				cell = block.position
 
-				siding = False
+				if block.collidable:
+					siding = False
 
-				if "l" in block.prop and self.position.right >= cell.left and last.right <= cell.left:
-					self.position.right = cell.left
-					siding = True
-					if self.acceleration.x > 0:
-						self.acceleration.x = 0
+					if "l" in block.prop and self.position.right >= cell.left and last.right <= cell.left:
+						self.position.right = cell.left
+						siding = True
+						if self.acceleration.x > 0:
+							self.acceleration.x = 0
 
-				if "r" in block.prop and self.position.left <= cell.right and last.left >= cell.right:
-					self.position.left = cell.right
-					siding = True
-					if self.acceleration.x < 0:
-						self.acceleration.x = 0
+					if "r" in block.prop and self.position.left <= cell.right and last.left >= cell.right:
+						self.position.left = cell.right
+						siding = True
+						if self.acceleration.x < 0:
+							self.acceleration.x = 0
 
-				if "u" in block.prop and self.position.bottom >= cell.top and last.bottom <= cell.top and not siding:
-					self.position.bottom = cell.top
-					self.resting = True
-					if self.acceleration.y > 0:
-						self.acceleration.y = 0
+					if "u" in block.prop and self.position.bottom >= cell.top and last.bottom <= cell.top and not siding:
+						self.position.bottom = cell.top
+						self.resting = True
+						if self.acceleration.y > 0:
+							self.acceleration.y = 0
 
-				if "d" in block.prop and self.position.top <= cell.bottom and last.top >= cell.bottom and not siding:
-					self.position.top = cell.bottom
-					if self.acceleration.y < 0:
-						self.acceleration.y = 0
+					if "d" in block.prop and self.position.top <= cell.bottom and last.top >= cell.bottom and not siding:
+						self.position.top = cell.bottom
+						if self.acceleration.y < 0:
+							self.acceleration.y = 0
+
+				if "s" in block.prop:
+					self.speedModifier = 0.2
+
+				if "w" in block.prop:
+					self.swimming = True
+					self.speedModifier = 0.5
+
+				if "k" in block.prop:
+					self.spawn()
 
 	def draw(self, screen):
 		self.sprites.draw(screen)
