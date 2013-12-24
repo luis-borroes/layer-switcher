8f8610910e311e5be2419ef7b7e1fc6857f7f106
Pedro <pedroborroes@gmail.com>
2013-12-24 02:02:26 +0000

    made prettier

--- setup.py
+++ setup.py
@@ -13,25 +13,25 @@ def isSystemDLL(pathname):
 	return origIsSystemDLL(pathname)
 py2exe.build_exe.isSystemDLL = isSystemDLL
 
-dist_dir = os.path.join('dist')
+dist_dir = os.path.join("dist")
 data_dir = dist_dir
 
 class Target:
-	script = 'main.pyw'
-	dest_base = 'layerswitcher'
-	icon_resources = [(0, os.path.join('assets', 'icons', 'icon.ico'))]
+	script = "main.pyw"
+	dest_base = "layerswitcher"
+	icon_resources = [(0, os.path.join("assets", "icons", "icon.ico"))]
 
 setup(
-	options = {'py2exe': {'dist_dir': dist_dir, 'bundle_files': 1, "compressed" : 1, "dll_excludes": ["w9xpopen.exe"]}},
+	options = {"py2exe": {"dist_dir": dist_dir, "bundle_files": 1, "compressed" : 1, "dll_excludes": ["w9xpopen.exe"]}},
 	zipfile = None,
 	windows = [Target],
 )
 
 data = []
-for dirpath, dirnames, filenames in os.walk('assets'):
+for dirpath, dirnames, filenames in os.walk("assets"):
 	data.extend(os.path.join(dirpath, fn) for fn in filenames)
 
-for dirpath, dirnames, filenames in os.walk('maps'):
+for dirpath, dirnames, filenames in os.walk("maps"):
 	data.extend(os.path.join(dirpath, fn) for fn in filenames)
 
 dest = data_dir
