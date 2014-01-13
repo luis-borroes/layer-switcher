ca952512aa4e9d86e238461028aff1be3e8d50c0
Pedro <pedroborroes@gmail.com>
2014-01-13 22:53:09 +0000

    added different enemy types and updated ai

--- enemy.py
+++ enemy.py
@@ -4,35 +4,92 @@ from vector import Vec2d as Vector
 
 class Enemy(character.Character):
 	group = []
+	blue = "enemyBlue"
+	yellow = "enemyYellow"
+	red = "enemyRed"
 
-	def __init__(self, game, gMap, pos, layer):
-		super(Enemy, self).__init__(game, gMap, "enemy", pos, layer)
+	def __init__(self, game, gMap, enemyType, pos, layer):
+		super(Enemy, self).__init__(game, gMap, enemyType, pos, layer)
 
-		self.inViewport = False
+		self.inProximity = False
+
+		if self.type in [Enemy.yellow, Enemy.red]:
+			self.moveSpeed = 400
+			self.jumpSpeed = -400
+			self.jumpTimerLimit = 0.25
+			self.swimSpeed = -250
 
 		Enemy.group.append(self)
 
 	def spawn(self):
 		super(Enemy, self).spawn()
 
-		self.holdJump = True
-
 	def die(self):
 		self.spawn()
 
 	def update(self, game, dt):
-		self.inViewport = self.position.centerx in xrange(game.viewport.rect.left, game.viewport.rect.right) and self.position.centery in xrange(game.viewport.rect.top, game.viewport.rect.bottom)
+		in_x = self.position.centerx - game.player.position.centerx in xrange(-game.resolution[0] / 2, game.resolution[0] / 2)
+		in_y = self.position.centery - game.player.position.centery in xrange(-game.resolution[1] / 2, game.resolution[1] / 2)
+		self.inProximity = in_x and in_y
 
-		if self.inViewport and self.layer == game.player.layer:
+		if self.type == Enemy.blue:
+			if self.inProximity and self.layer == game.player.layer:
 
-			if game.player.position.centerx < self.position.centerx:
-				self.moveLeft(dt)
+				if game.player.position.centerx < self.position.centerx:
+					self.moveLeft(dt)
 
-			elif game.player.position.centerx > self.position.centerx:
-				self.moveRight(dt)
+				elif game.player.position.centerx > self.position.centerx:
+					self.moveRight(dt)
 
-			if game.player.position.centery < self.position.centery and not self.jumping and self.resting:
-				self.jump()
+				if game.player.position.centery < self.position.centery:
+					self.holdJump = True
+
+					if self.resting:
+						self.jump()
+
+				if game.player.position.centery > self.position.centery and self.holdJump:
+					self.holdJump = False
+
+		if self.type == Enemy.yellow:
+			if self.inProximity:
+
+				if game.player.position.centerx < self.position.centerx:
+					self.moveLeft(dt)
+
+				elif game.player.position.centerx > self.position.centerx:
+					self.moveRight(dt)
+
+				if game.player.position.centery < self.position.centery:
+					self.holdJump = True
+
+					if self.resting:
+						self.jump()
+
+				if game.player.position.centery > self.position.centery and self.holdJump:
+					self.holdJump = False
+
+		if self.type == Enemy.red:
+			if self.inProximity:
+
+				if game.player.layer > self.layer:
+					self.toFront(game)
+				elif game.player.layer < self.layer:
+					self.toBack(game)
+
+				if game.player.position.centerx < self.position.centerx:
+					self.moveLeft(dt)
+
+				elif game.player.position.centerx > self.position.centerx:
+					self.moveRight(dt)
+
+				if game.player.position.centery < self.position.centery:
+					self.holdJump = True
+
+					if self.resting:
+						self.jump()
+
+				if game.player.position.centery > self.position.centery and self.holdJump:
+					self.holdJump = False
 
 		super(Enemy, self).update(game, dt)
 
